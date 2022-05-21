b67555e0e7f3c01a4f93cbea43ec1c35b89657ed
Likhith68 <Likhithjsc@gmail.com>
2022-05-21 18:38:15 +0530

    First Commit-Uploading from Local Machine

---
+++ src/Components/Testimonials.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+const Testimonials = ({ data }) => {
+  if (data) {
+    var testimonials = data.testimonials.map(function (testimonials) {
+      return (
+        <li key={testimonials.url}>
+          <blockquote>
+            <p>{testimonials.text}</p>
+          </blockquote>
+        </li>
+      );
+    });
+  }
+
+  return (
+    <section id="testimonials">
+      <div className="text-container">
+        <div className="row">
+          <div className="two columns header-col">
+            <h1>
+              <span>Client Testimonials</span>
+            </h1>
+          </div>
+
+          <div className="ten columns flex-container">
+            <ul className="slides">{testimonials}</ul>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Testimonials;
